8e2d9ab9dc4bb1734563b5728fa2597885cbb4a8
cjaf1994 <cristian.aricapa.mt@usa.edu.co>
2022-10-06 20:27:30 -0400

    First commit proyecto maven

---
+++ reto3team/src/main/java/com/example/reto3team/Reto3teamApplication.java
@@ -0,0 +1,13 @@
+package com.example.reto3team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto3teamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto3teamApplication.class, args);
+	}
+
+}
